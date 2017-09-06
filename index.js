41058ad613050172ec320fc613db0abda18b6ce7
华仔_biubiubiu <452216418@qq.com>
2017-09-06 17:39:25 +0800

    增加了图片浏览

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by ZWH on 2017/6/22.
+ */
+import CalendarComponent from './user.vue'
+// 导出组件 这里是重点
+
+export default {
+  install (Vue) {
+    Vue.component('Calendar', Vue.extend(CalendarComponent));
+  }
+}
+
