16c79bcf85568decf9eb9098e0d32a6ca2458614
zwh <zwh@zwhdeMacBook-Pro.local>
2017-12-15 22:02:29 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
+import Vue from 'vue';
+import App from './App';
+import router from './router';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 import Calendar from 'vue-calendar-component';
 Vue.use(Calendar);
 
 /* eslint-disable no-new */
 new Vue({
-    el: '#app',
-    router,
-    template: '<App/>',
-    components: { App }
-})
+  el: '#app',
+  router,
+  template: '<App/>',
+  components: { App }
+});
