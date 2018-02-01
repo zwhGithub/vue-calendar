623446a148483ae697e861e3ff5f3dfc86f7afa4
zwhGithub <452216418@qq.com>
2018-02-01 19:24:20 +0800

    更好的支持pc端

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -1,39 +1,40 @@
 <template>
-  <div class="content">
-    <!-- agoDayHide='1514937600000' -->
-    <Calendar ref="Calendar" v-on:choseDay="clickDay" v-on:isToday="clickToday" v-on:changeMonth="changeDate" :markArray=[20,21] ></Calendar>
-    <br/>
-    <h1 @click="demo">标记了20号21号</h1>
-  </div>
+    <div class="content">
+        <!-- agoDayHide='1517483961000' -->
+        <!-- isHideOtherday=false -->
+        <Calendar ref="Calendar" v-on:choseDay="clickDay" v-on:isToday="clickToday" v-on:changeMonth="changeDate" :markArray=[20,21]></Calendar>
+        <br/>
+        <h1 @click="demo">标记了20号21号</h1>
+    </div>
 </template>
 
 <script>
-export default {
-    data() {
-        return {}
-    },
-    methods: {
-        clickDay(data) {
-            console.log(data) //选中某天
-            this.$toast('选中了' + data)
+    export default {
+        data() {
+            return {}
         },
-        clickToday(data) {
-            console.log(data) //跳到了本月
-        },
-        changeDate(data) {
-            this.$toast('切换到的月份为' + data)
-            console.log(data) //左右点击切换月份
-        },
-        demo() {
-            //this.$refs.Calendar.ChoseMonth('2017-01');
+        methods: {
+            clickDay(data) {
+                console.log(data) //选中某天
+                this.$toast('选中了' + data)
+            },
+            clickToday(data) {
+                console.log(data) //跳到了本月
+            },
+            changeDate(data) {
+                this.$toast('切换到的月份为' + data)
+                console.log(data) //左右点击切换月份
+            },
+            demo() {
+                //this.$refs.Calendar.ChoseMonth('2017-01');
+            }
         }
     }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1 {
-    text-align: center;
-}
+    h1 {
+        text-align: center;
+    }
 </style>
