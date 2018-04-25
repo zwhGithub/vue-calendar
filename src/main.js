d00e00f46ae88767f9d444d5d76c10341d0fe97b
zwhGithub <452216418@qq.com>
2018-04-25 23:19:44 +0800

    全面更改调用方式 优化了标记的方式
    fix
    
    优化提示
    
    完善了文档

--- src/main.js
+++ src/main.js
@@ -8,10 +8,6 @@ Vue.config.productionTip = false;
 import Toast from 'vue-toast-component';
 Vue.use(Toast);
 
-// import Calendar from 'vue-calendar-component';
-import Calendar from './vue-calendar-component/index';
-Vue.use(Calendar);
-
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/vue-calendar-component/index.js
+++ src/vue-calendar-component/index.js
@@ -1,14 +1,5 @@
 /**
  * Created by ZWH on 2017/6/22.
  */
-import CalendarComponent from './calendar.vue';
-// 导出组件 这里是重点
-
-export default {
-  install(Vue) {
-    Vue.component('Calendar', Vue.extend(CalendarComponent));
-  }
-};
-
-let a = ['2018/4/4', '2018/4/6'];
-console.log(a);
+import Calendar from './calendar.vue';
+export default Calendar;
