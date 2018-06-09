56260b0e367dc683177387075304f28dd07c2a39
zwhGithub <452216418@qq.com>
2018-06-09 09:05:14 +0800

    去掉一个api，重新样式编写

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -1,101 +1,109 @@
 <template>
-    <div class="content">
-        <!-- agoDayHide='1517483961' -->
-        <!-- futureDayHide="1526054400" -->
-        <!-- isHideOtherday=false -->
-        <Calendar ref="Calendar" :markDateMore="arr" v-on:choseDay="clickDay" v-on:changeMonth="changeDate"></Calendar>
-        <br/>
-        <h3 @click="demo">markDateMore标记不同风格：：1号2号一种风格====13号另一种风格</h3>
-        <br>
-        <h3>markDate 标记了单一风格 更简单</h3>
-        <div class="div" @click="demo ">
-            点击跳到2017-12-12
-        </div>
+  <div class="content">
+    <!-- agoDayHide='1517483961' -->
+    <!-- futureDayHide="1526054400" -->
+    <Calendar ref="Calendar" :markDateMore="arr" v-on:isToday="clickToday" agoDayHide=1530115200 v-on:choseDay="clickDay" v-on:changeMonth="changeDate"></Calendar>
+    <br/>
+    <h3 @click="demo">markDateMore标记不同风格：：1号2号一种风格====13号另一种风格</h3>
+    <br>
+    <h3>markDate 标记了单一风格 更简单</h3>
+    <div class="div" @click="demo ">
+      点击跳到2018-12-12
     </div>
+  </div>
 </template>
 
 <script>
-    // import Calendar from '../vue-calendar-component/index';
-    import Calendar from 'vue-calendar-component';
-    export default {
-        data() {
-            return {
-                arr: [{
-                    date: '2018/4/1',
-                    className: "mark1"
-                }, {
-                    date: '2018/4/2',
-                    className: "mark1"
-                }, {
-                    date: '2018/4/13',
-                    className: "mark2"
-                }]
-            }
+// import Calendar from '../vue-calendar-component/index';
+import Calendar from 'vue-calendar-component';
+export default {
+  data() {
+    return {
+      markTim: ['2018/6/1'],
+      arr: [
+        {
+          date: '2018/6/1',
+          className: 'mark1'
         },
-        components: {
-            Calendar
+        {
+          date: '2018/6/2',
+          className: 'mark1'
         },
-        methods: {
-            clickDay(data) {
-                console.log('选中了', data) //选中某天
-                this.$toast('选中了' + data)
-            },
-            clickToday(data) {
-                console.log('跳到了本月', data) //跳到了本月
-            },
-            changeDate(data) {
-                this.$toast('切换到的月份为' + data)
-                console.log('左右点击切换月份', data) //左右点击切换月份
-            },
-            demo() {
-                // this.$refs.Calendar.ChoseMonth('2017-12-12',false); //跳到12月12日 但是不选中12月12日
-                this.$refs.Calendar.ChoseMonth('2017-12-12'); //跳到12月12日
-            }
-        },
-        created() {
-            function format(date, index) {
-                date = new Date(date);
-                return `${date.getFullYear()}/${date.getMonth() + 1}/${index}`;
-            }
-            this.arr = [{
-                date: format(new Date(), 1),
-                className: "mark1"
-            }, {
-                date: format(new Date(), 2),
-                className: "mark1"
-            }, {
-                date: format(new Date(), 13),
-                className: "mark2"
-            }]
+        {
+          date: '2018/6/13',
+          className: 'mark2'
         }
+      ]
+    };
+  },
+  components: {
+    Calendar
+  },
+  methods: {
+    clickDay(data) {
+      console.log('选中了', data); //选中某天
+      this.$toast('选中了' + data);
+    },
+    clickToday(data) {
+      console.log('跳到了本月今天', data); //跳到了本月
+    },
+    changeDate(data) {
+      this.$toast('切换到的月份为' + data);
+      console.log('左右点击切换月份', data); //左右点击切换月份
+    },
+    demo() {
+      this.$refs.Calendar.ChoseMonth('2018-12-12'); //跳到12月12日 但是不选中12月12日
+      // this.$refs.Calendar.ChoseMonth('2018-12-12'); //跳到12月12日
     }
+  },
+  created() {
+    function format(date, index) {
+      date = new Date(date);
+      return `${date.getFullYear()}/${date.getMonth() + 1}/${index}`;
+    }
+    this.arr = [
+      {
+        date: format(new Date(), 1),
+        className: 'mark1'
+      },
+      {
+        date: format(new Date(), 2),
+        className: 'mark1'
+      },
+      {
+        date: format(new Date(), 13),
+        className: 'mark2'
+      }
+    ];
+  }
+};
 </script>
 
 <!-- Add "scoped " attribute to limit CSS to this component only -->
 <style>
-    h3 {
-        text-align: center;
-        width: 90%;
-        margin: auto;
-    }
+h3 {
+  text-align: center;
+  width: 90%;
+  margin: auto;
+}
 
-    .div {
-        margin: auto;
-        width: 220px;
-        height: 44px;
-        line-height: 44px;
-        background: #0fc37c;
-        color: #fff;
-        font-size: 17px;
-        text-align: center;
-        margin-top: 20px;
-    }
+.div {
+  margin: auto;
+  width: 220px;
+  height: 44px;
+  line-height: 44px;
+  background: #0fc37c;
+  color: #fff;
+  font-size: 17px;
+  text-align: center;
+  margin-top: 20px;
+}
 
-    .mark1 {
-        background-color: red;
-    }
+.mark1 {
+  background-color: red;
+}
 
-    .mark2 {
-        background-color: blue;
-    }
+.mark2 {
+  background-color: blue;
+}
 </style>
