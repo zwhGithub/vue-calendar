bf50ac6940683127f79e50a637348fe2abab195a
华仔 <zwhcoder@gmail.com>
2019-01-29 19:00:44 +0800

    fix

--- src/demo/demo.vue
+++ src/demo/demo.vue
@@ -7,7 +7,7 @@
       :markDateMore="arr"
       :markDate="arr2"
       v-on:isToday="clickToday"
-      agoDayHide="1530115200"
+      agoDayHide="1530115220"
       v-on:choseDay="clickDay"
       v-on:changeMonth="changeDate"
     ></Calendar>
