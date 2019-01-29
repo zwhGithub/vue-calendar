d8cb45cac46d8b82cc99bc242bbef3c7d76a02b1
华仔 <zwhcoder@gmail.com>
2019-01-29 18:59:27 +0800

    fix

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -37,7 +37,7 @@ const webpackConfig = merge(baseWebpackConfig, {
       compress: {
         warnings: false
       },
-      sourceMap: true
+      sourceMap: false
     }),
     // extract css into its own file
     new ExtractTextPlugin({
